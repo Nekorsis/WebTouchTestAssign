9221dbd2171f4867b3389068c842ff0003399acf
Nekorsis <alexche9456@gmail.com>
2018-10-29 14:04:50 +0700

    Updates

---
+++ precache-manifest.441f118d848e8eac19a59c40ba30049a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "878e616573b7edde378e",
+    "url": "/WebTouchTestAssign/static/js/runtime~main.878e6165.js"
+  },
+  {
+    "revision": "1a117189e723262c5647",
+    "url": "/WebTouchTestAssign/static/js/main.1a117189.chunk.js"
+  },
+  {
+    "revision": "9c077cda0315cf7f9781",
+    "url": "/WebTouchTestAssign/static/js/1.9c077cda.chunk.js"
+  },
+  {
+    "revision": "1a117189e723262c5647",
+    "url": "/WebTouchTestAssign/static/css/main.49b17613.chunk.css"
+  },
+  {
+    "revision": "6e31527950d85d08802fee6cf7e66ab4",
+    "url": "/WebTouchTestAssign/index.html"
+  }
+];
